8438b10e197a9859c9364a6fafe8d38ec337ad96
shaeif <shaeifaj@gmail.com>
2021-03-27 17:37:24 +0300

    version 1.1

--- devops.py
+++ devops.py
@@ -7,36 +7,59 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 
-cmd = sys.argv[1]
-fileName = sys.argv[2]
-githubEmail = ""
-githunUsername = ""
-githubPassword = ""
+fileName = sys.argv[1]
+
+githubEmail = "shaeifaj@gmail.com"
+githunUsername = "shaeif"
+githubPassword = "open@github1594"
 
 def createRepo(fileName):
-    #Creating Repo
     driver = webdriver.Chrome()
-    driver.get('[https://github.com/login](https://github.com/login)')
+    driver.get('https://github.com/login')
     driver.find_element_by_xpath('/html/body/div[3]/main/div/div[4]/form/input[2]').send_keys(githubEmail)
     driver.find_element_by_xpath('/html/body/div[3]/main/div/div[4]/form/div/input[1]').send_keys(githubPassword)
     driver.find_element_by_xpath('/html/body/div[3]/main/div/div[4]/form/div/input[12]').send_keys(Keys.RETURN)
-    driver.get('[https://github.com/new](https://github.com/new)')
+    driver.get('https://github.com/new')
     driver.find_element_by_xpath('/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input').send_keys(fileName)
     time.sleep(1)
     driver.find_element_by_xpath('/html/body/div[4]/main/div/form/div[4]/button').send_keys(Keys.RETURN)
     driver.quit()
-    #Setting enviroment
-    os.chdir("/home/shaeif/workspace")
-    os.mkdir(fileName)
-    os.chdir(fileName)
-    #Setting Repo
-    os.system(f'echo "# {fileName}" >> [README.md](http://readme.md/)')
+
+def createLocalRepo():
+    os.system(f'echo "# {fileName}" >> README.md')
     os.system('git init')
-    os.system('git add [README.md](http://readme.md/)')
+    os.system('git add README.md ')
     os.system('git commit -m "first commit"')
     os.system('git branch -M main')
     time.sleep(2)
-    os.system(f'git remote add origin [https://github.com/{githunUsername}/{fileName}.git](https://github.com/%7BgithunUsername%7D/%7BfileName%7D.git)')
+    os.system(f'git remote add origin https://github.com/{githunUsername}/{fileName}.git')
     os.system('git push -u origin main')
     os.system('code .')
+
+def setUp():
+    os.mkdir(fileName)
+    os.chdir(fileName)
+
+def start(fileName):
+    os.chdir("/home/shaeif/workspace")
+    options = ["0. Normal","1. Angular","2. laravel","3. React","4. React-Native"]
+    for x in options:
+        print(x)
+    o = input("Enter Choice \n")
+    if o == "1" or o == "Angular": os.system(f'ng new {fileName}') 
+    elif o == "0" or o == "Normal": os.mkdir(fileName)
+    elif o == "2" or o == "laravel": os.system(f'composer create-project laravel/laravel {fileName}')
+    elif o == "3" or o == "React": os.system(f'npx create-react-app {fileName}')
+    elif o == "4" or o == "React-native" : os.system(f'expo init {fileName}')
+    else : 
+        print("Enter a valid option \n")
+        start(fileName)
+    os.chdir(fileName)
+    createLocalRepo()
 createRepo(fileName)
+start(fileName)
+        
+
+
+
+
